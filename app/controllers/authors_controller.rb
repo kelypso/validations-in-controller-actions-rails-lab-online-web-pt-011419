769647a4e02fc48d94563c70d25520f98e30e95d
Kelsey Low <kklowlow@gmail.com>
2019-06-06 02:20:27 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,12 +7,12 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create!(author_params)
-    if @author.valid? 
+    @author = Author.new(author_params)
+    if @author.valid?
       @author.save
       redirect_to author_path(@author)
-    else 
-      render :new 
+    else
+      render :new
     end
   end
 
